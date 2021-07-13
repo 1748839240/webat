0271d62290d89eec23976cedd58315f09a084a27
yinchengnuo <yinchengnuo@qq.com>
2021-07-13 11:04:00 +0800

    完成

--- src/components/At/At.vue
+++ src/components/At/At.vue
@@ -9,7 +9,7 @@
           <li>光标不可游走 @名称 其中</li>
           <li>@名称 只能整体被选中</li>
           <li>@选择人员实时映射到数据</li>
-          <li>XSS注意</li>
+          <li>XSS注意同时仅允许用户通过输入、粘贴饿形式修改输入内容（通过console、element面板修改无效）</li>
         </ul>
       </el-card>
     </div>
@@ -27,17 +27,18 @@
                 <template #header>
                   <div class="flex flex_sb">
                     <span>@谁</span>
-                    <i class="el-icon-close" @click="closePopover" />
+                    <i class="el-icon-close" @click="close" />
                   </div>
                 </template>
                 <div v-for="item in unAtList" :key="item.id" class="flex flex_sb bb p8">
                   <span>{{ item.name }}</span>
-                  <el-button size="mini" @click="selectAt(item)">选择</el-button>
+                  <el-button size="mini" @click="select(item)">选择</el-button>
                 </div>
               </el-card>
             </template>
             <template #reference>
-              <div id="atInput" ref="refAtInput" contenteditable @input="inputing" @click="click" />
+              <div id="atInput" ref="refAtInput" contenteditable="plaintext-only" @input="input"
+                @focus="state.visible = false" @click="click" />
             </template>
           </el-popover>
           <div v-if="!state.inputing" class="placeholder" @click.stop="() => refAtInput.focus()">
@@ -56,7 +57,6 @@
 
 <script lang="ts">
 import list, { Item } from "./list"
-import { insertAfter, isNextSiblings, isPreviousSiblings } from './util'
 import { ref, reactive, computed, nextTick, onMounted, defineComponent, onBeforeUnmount } from "vue"
 
 const className = '__at_span'
@@ -65,42 +65,118 @@ export default defineComponent({
   setup() {
     const refAtInput = ref()
     const state = reactive({
-      list, // 人名字列表
       inputing: "", // 实时输入内容
-      inputText: "", // 展示的文字
-      focusOffset: 0, // 缓存光标位置
+      focusNode: {} as Node, // 缓存光标所在节点
+      focusOffset: 0, // 缓存光标所在位置
+      inputed: false,// 输入框内容是否是常规输入
       visible: false, // 是否显示选择人员弹窗
       observer: {} as MutationObserver, // dom 监听器
       atIds: [] as Array<string>, // @ 的人员id
     })
 
     // 可选人员列表
-    const atedList = computed(() => state.list.filter((e) => state.atIds.includes(e.id)))
+    const atedList = computed(() => list.filter((e) => state.atIds.includes(e.id)))
 
     // 已选人员列表
-    const unAtList = computed(() => state.list.filter((e) => !state.atIds.includes(e.id)))
+    const unAtList = computed(() => list.filter((e) => !state.atIds.includes(e.id)))
+
+    // lock，用于标记内容修改是在常规修改（在输入框输入或粘贴）还是通过其他方式修改（浏览器控制台修改）
+    const lock = () => {
+      state.inputed = true
+      nextTick((() => {
+        state.inputed = false
+      }))
+    }
+
+    // 打开选择框
+    const open = () => {
+      const selection = window.getSelection() as Selection
+      const range = selection?.getRangeAt(0) as Range
+      // 缓存光标所在节点
+      state.focusNode = selection?.focusNode as Node
+      // 缓存光标所在节点位置
+      state.focusOffset = selection?.focusOffset as number
+      // 光标所在位置
+      const pos = range.getBoundingClientRect() as DOMRect
+      // 显示选择框
+      state.visible = true
+      // 输入框失去焦点
+      refAtInput.value.blur()
+      // 设置弹窗位置
+      nextTick(() => {
+        const x = pos.x > 75 ? pos.x - 75 : 0
+        const y = pos.y + pos?.height + 8
+        const s = document.getElementsByClassName("atPopover")[0] as HTMLElement
+        s.style.transform = `translate3d(${x}px, ${y}px, 0px)`
+      })
+    }
+
+    // 关闭选择框
+    const close = () => {
+      // 关闭选择框
+      state.visible = false
+      const selection = window.getSelection()
+      const range = selection?.getRangeAt(0)
+      // 选中节点
+      range?.selectNode(state.focusNode)
+      // 设置终点
+      range?.setEnd(state.focusNode, state.focusOffset)
+      // 移动到终点
+      range?.collapse()
+      // 聚焦输入框
+      refAtInput.value.focus()
+    }
+
+    // 选择 @
+    const select = (item: Item) => {
+      const selection = window.getSelection()
+      const range = selection?.getRangeAt(0)
+      // 选中输入的 @ 符
+      range?.setStart(state.focusNode, state.focusOffset - 1)
+      range?.setEnd(state.focusNode, state.focusOffset)
+      // 删除输入的 @ 符
+      range?.deleteContents()
+      // 创建元素节点
+      const element = document.createElement('SPAN')
+      element.className = className
+      element.dataset.id = item.id
+      element.contentEditable = 'false'
+      element.innerText = `@${item.name}`
+      // 选中元素节点
+      range?.insertNode(element)
+      // 光标移动到末尾
+      range?.collapse()
+      // 聚焦输入框
+      refAtInput.value.focus()
+      // 关闭选择框
+      state.visible = false
+      // 标记为常规修改
+      lock()
+    }
 
     // 输入监听
-    const inputing = (e: InputEvent) => {
-      // 保存实时输入信息用于控制 placeholder 状态
+    const input = (e: InputEvent) => {
+      // 标记内容修改为常规输入
+      lock()
+      // 实时输入框的保存
       state.inputing = (e.target as Element).innerHTML
-      // 如果正在拼音输入，退出函数
-      if (e.inputType === "insertCompositionText") {
-        return
-      }
       // 如果触发 @
       if (e.data === "@") {
-        // // 缓存光标位置
-        state.focusOffset = window.getSelection()?.focusOffset as number
         // 打开选择弹窗
-        openPopover()
-        return
-      } else {
-        state.visible = false
+        open()
       }
     }
 
-    // // 粘贴处理（ webkit 浏览器可以用 contenteditable="plaintext-only" 设置 contenteditable 粘贴文字，此方法可以用于火狐浏览器 hack）
+    // 点击 @ 选中
+    const click = ({ target }: MouseEvent) => {
+      if ((target as HTMLElement).className === className) {
+        const selection = window.getSelection()
+        const range = selection?.getRangeAt(0)
+        range?.selectNode(target as Node)
+      }
+    }
+
+    // 粘贴处理（ webkit 浏览器可以用 contenteditable="plaintext-only" 设置 contenteditable 粘贴文字，此方法可以用于火狐浏览器 hack，缺点 Ctrl + Z 无效）
     // const paste = (e: ClipboardEvent) => {
     //   // 获取到粘贴的纯文本
     //   const data = e.clipboardData?.getData("text/plain")
@@ -120,104 +196,55 @@ export default defineComponent({
     //   // 取消选中状态将光标移动至选中最后
     //   selection?.collapseToEnd()
     //   // 触发 input 保存文本数据
-    //   inputing(e as unknown as InputEvent)
+    //   state.inputing = (e.target as HTMLElement).innerHTML
     //   // 阻止默认事件
     //   e.preventDefault()
     //   return false
     // }
 
-    // 打开 Popover
-    const openPopover = () => {
-      // 获取当前光标位置
-      const curporPos = window.getSelection()?.getRangeAt(0).getBoundingClientRect() as DOMRect
-      // 打开弹出框
-      state.visible = true
-      // 设置弹窗位置
-      nextTick(() => {
-        const x = curporPos.x > 75 ? curporPos.x - 75 : 0
-        const y = curporPos.y + curporPos?.height + 8
-          ; (document.getElementsByClassName("atPopover")[0] as HTMLElement).style.transform = `translate3d(${x}px, ${y}px, 0px)`
-      })
-    }
-
-    // 关闭 Popover
-    const closePopover = () => {
-      // 关闭弹窗
-      state.visible = false
-      // 自动聚焦光标为移动到首位
-      refAtInput.value.focus()
-      // 设置 selection 选中到缓存的位置
-      window.getSelection()?.extend(window.getSelection()?.anchorNode as Node, state.focusOffset)
-      // 将光标移动到末尾
-      window.getSelection()?.getRangeAt(0).collapse(false)
-    }
-
-    // 选择 @
-    const selectAt = (item: Item) => {
-      // 关闭选择弹窗
-      closePopover()
-      // 为即将插入的 span 生成一个 id，便于后面找到
-      const id = Date.now().toString()
-      // @ 人员信息
-      // 要插入的元素
-      const selection = window.getSelection()
-      const parent = selection?.focusNode?.parentElement as HTMLElement
-      // 选中输入的 @ 符
-      selection?.extend(selection?.focusNode as Node, state.focusOffset - 1);
-      // 删除输入的 @ 符
-      const range = selection?.getRangeAt(0)
-      range?.deleteContents()
-      // 向文本节点插入元素节点
-      const element = document.createElement('SPAN')
-      element.id = id
-      element.className = className
-      element.dataset.id = item.id
-      element.contentEditable = 'false'
-      element.innerText = `@${item.name}`
-      range?.insertNode(element)
-      selection?.collapseToEnd()
-    }
-
     onMounted(() => {
       // 自动聚焦
       refAtInput.value.focus()
-      state.observer = new MutationObserver(function (mutationsList) {
-        // 添加的元素
-        const addElements = Array.from(mutationsList.map(e => Array.from(e.addedNodes))).flat().filter(e => (e as HTMLElement).tagName) as Array<HTMLElement>
-        // 添加的 @姓名 元素
-        const addAtIds = addElements.filter(e => (e.tagName === 'SPAN') && (e.className === className)).map(e => e.dataset.id) as Array<string>
-        // 添加的其他元素
-        const addOthers = addElements.filter(e => !((e.tagName === 'SPAN') && (e.className === className)))
-        // 删除其他元素防止XSS攻击
-        // addOthers.forEach(element => element.remove())
-        // 添加 @
-        addAtIds.forEach(id => !state.atIds.includes(id) && state.atIds.push(id))
-        // 删除的元素  
-        const delElements = Array.from(mutationsList.map(e => Array.from(e.removedNodes))).flat().filter(e => (e as HTMLElement).tagName) as Array<HTMLElement>
-        const delAtIds = delElements.filter(e => (e.tagName === 'SPAN') && (e.className === className)).map(e => e.dataset.id) as Array<string>
-        delAtIds.forEach(id => (state.atIds.indexOf(id) > -1) && state.atIds.splice(state.atIds.indexOf(id), 1))
+      state.observer = new MutationObserver(mutationsList => {
+        if (state.inputed) {
+          // 添加的元素
+          const addElements = Array.from(mutationsList.map(e => Array.from(e.addedNodes))).flat().filter(e => (e as HTMLElement).tagName) as Array<HTMLElement>
+          // 添加的 @姓名 元素
+          const addAtIds = addElements.filter(e => (e.tagName === 'SPAN') && (e.className === className)).map(e => e.dataset.id) as Array<string>
+          // 添加 @
+          addAtIds.forEach(id => !state.atIds.includes(id) && state.atIds.push(id))
+          // 删除的元素  
+          const delElements = Array.from(mutationsList.map(e => Array.from(e.removedNodes))).flat().filter(e => (e as HTMLElement).tagName) as Array<HTMLElement>
+          const delAtIds = delElements.filter(e => (e.tagName === 'SPAN') && (e.className === className)).map(e => e.dataset.id) as Array<string>
+          delAtIds.forEach(id => (state.atIds.indexOf(id) > -1) && state.atIds.splice(state.atIds.indexOf(id), 1))
+        } else {
+          alert('非法修改')
+          window.location.reload()
+        }
       })
-      state.observer.observe(refAtInput.value, { subtree: true, childList: true })
+      state.observer.observe(refAtInput.value, {
+        subtree: true, // 监听所有后代节点
+        childList: true, // 监听后代节点增加删除
+        attributes: true, // 监听属性
+        characterData: true, // 监听字符数据变化
+        attributeOldValue: true, // 记录变化前属性值
+        characterDataOldValue: true, // 记录变化前字符值
+      })
     })
 
     onBeforeUnmount(() => {
       state.observer.disconnect()
     })
 
-    const click = (e: MouseEvent) => {
-      // if ((e.target as HTMLElement).className = className) {
-      //   window.getSelection()?.getRangeAt(0).selectNode(e.target as HTMLElement)
-      // }
-    }
     return {
       state,
+      close,
+      input,
       click,
+      select,
       atedList,
       unAtList,
-      selectAt,
-      inputing,
       refAtInput,
-      closePopover,
     }
   },
 })
